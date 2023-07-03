2e8b211436c8018b0a1e03918bc1a4fe6af51d6f
Cindy Xiao <git@cxiao.net>
2023-07-03 14:21:46 -0700

    feat: Return list of string slices from recovery function

--- __init__.py
+++ __init__.py
@@ -2,15 +2,29 @@ from binaryninja.binaryview import BinaryView, DataVariable
 from binaryninja.log import Logger
 from binaryninja.types import IntegerType, PointerType
 
-from typing import List
+from dataclasses import dataclass
+from pprint import pformat
+from typing import List, Optional
 
 logger = Logger(session_id=0, logger_name="rust_string_slicer")
 
 
-def recover_string_slices_from_readonly_data(bv: BinaryView):
+@dataclass
+class StringSlice:
+    address: int
+    length: int
+    data: bytes
+
+    def __repr__(self):
+        return f"StringSlice(address={self.address:#x}, length={self.length:#x}, data={self.data})"
+
+
+def recover_string_slices_from_readonly_data(
+    bv: BinaryView,
+) -> Optional[List[StringSlice]]:
     if bv.arch is None:
         logger.log_error("Could not get architecture of current binary view, exiting")
-        return
+        return None
 
     readonly_segments = list(
         filter(
@@ -20,6 +34,9 @@ def recover_string_slices_from_readonly_data(bv: BinaryView):
             bv.segments,
         )
     )
+    if len(readonly_segments) == 0:
+        logger.log_error("Could not find any read-only segment in binary, exiting")
+        return None
 
     # Obtain all data vars which are pointers to data in readonly data segments
     data_vars_to_ro_segment_data: List[DataVariable] = []
@@ -32,9 +49,10 @@ def recover_string_slices_from_readonly_data(bv: BinaryView):
                         f"Found pointer var at {candidate_string_slice_data_ptr.address:#x} ({candidate_string_slice_data_ptr}) pointing to {candidate_string_slice_data_ptr.value:#x} "
                     )
 
-    # Try to read an integer following the data var,
-    # and treat it as a candidate for a string slice length.
+    recovered_string_slices: List[StringSlice] = []
     for candidate_string_slice_data_ptr in data_vars_to_ro_segment_data:
+        # Try to read an integer following the data var,
+        # and treat it as a candidate for a string slice length.
         candidate_string_slice_len_addr = (
             candidate_string_slice_data_ptr.address
             + candidate_string_slice_data_ptr.type.width
@@ -82,11 +100,22 @@ def recover_string_slices_from_readonly_data(bv: BinaryView):
             logger.log_info(
                 f'Recovered string at addr {candidate_string_slice_data_ptr.value:#x}, len {candidate_string_slice_len:#x}: "{candidate_utf8_string}"'
             )
+
+            # Append the final string slice object to the list of recovered strings.
+            recovered_string_slices.append(
+                StringSlice(
+                    address=candidate_string_slice_data_ptr.value,
+                    length=candidate_string_slice_len,
+                    data=candidate_string_slice,
+                )
+            )
         except UnicodeDecodeError as err:
             logger.log_warn(
-                "Candidate string slice {candidate_string_slice} does not decode to a valid UTF-8 string; excluding from final results: {err}"
+                f"Candidate string slice {candidate_string_slice} does not decode to a valid UTF-8 string; excluding from final results: {err}"
             )
             continue
 
+    return recovered_string_slices
 
-recover_string_slices_from_readonly_data(bv)
+
+logger.log_info(pformat(recover_string_slices_from_readonly_data(bv)))
